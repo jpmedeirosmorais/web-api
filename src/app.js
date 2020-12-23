6c6836f26be04dfdbd7e1fb855a23f54eb4ebdca
JoaoPaulo <paulomedeiros5000@gmail.com>
2020-12-23 13:09:56 -0300

    update

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require('express');
+
+const app = express();
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.set("port", port);
+
+app.use((request, response, next) =>{
+    response.status(404).send();
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.clear();
+    console.log("\n-------------------------------------------------");
+    console.log(`Servidor em execução em http://${hostname}:${port}/`);
+    console.log("-------------------------------------------------\n");
+});
